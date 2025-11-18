e6127371e7901bec0f8b4a069a1a12156c752dc5
ratulislam46 <arfanratul46@gmail.com>
2025-11-18 22:43:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ibgq1ve.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const userData = client.db('studen-hub').collection("userData");
+
+        app.post('/users-data', async (req, res) => {
+            const data = req.body;
+            // console.log(data);
+            const result = await userData.insertOne(data);
+            res.send(result)
+        })
+
+        app.get('/', (req, res) => {
+            res.send('6 senses server side is running.');
+        });
+
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}!`);
+        });
+    }
+    finally {
+    }
+}
+run().catch(console.dir);
